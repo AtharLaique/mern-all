b627acea3f16bbf37f6edd4362db42aaaecc5652
ATHAR LAIQUE <atharlaique15@gmail.com>
2020-10-21 11:23:52 +0500

    use index.js as router middelware

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express()
 require('dotenv').config()
 
 const port=process.env.PORT ||5001
-
+app.use('/api/',require('./src/routes'))
 app.listen(port,()=>{
   console.log(`Server is Running on ${port}`)
 })
