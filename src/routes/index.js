18d4611500403a043fdc6effa72eba7cdd73638e
ATHAR LAIQUE <atharlaique15@gmail.com>
2020-10-21 11:29:21 +0500

    home as middleware in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,5 @@ const router = express.Router()
 router.get('/hello',(req,res)=>{
   res.send("<h1>Hello From Express Server</h1>")
 })
-
+router.use('/home',require('./home/home'));
 module.exports=router;
